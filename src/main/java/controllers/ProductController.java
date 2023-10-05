e6d1aa0c4dd7b62bc74d2533048f1458af87fca5
Thang <nguyentrongthang10051999@gmail.com>
2023-10-05 09:19:50 +0700

    Created

--- src/main/java/controllers/ProductController.java
+++ src/main/java/controllers/ProductController.java
@@ -7,12 +7,18 @@ import services.BranchService;
 import services.ProductService;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import java.io.File;
 import java.io.IOException;
 import java.math.BigDecimal;
+@MultipartConfig(fileSizeThreshold = 1024 * 1024 * 2, // 2MB
+        maxFileSize = 1024 * 1024 * 10,      // 10MB
+        maxRequestSize = 1024 * 1024 * 50)  // 50MB
 
 @WebServlet(name = "productController", urlPatterns = "/product")
 public class ProductController extends HttpServlet {
@@ -103,8 +109,46 @@ public class ProductController extends HttpServlet {
         req.getRequestDispatcher("product/create.jsp").forward(req,resp);
     }
 
-    private void create(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        productService.create(getProductByRequest(req));
+    private void create(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        String productName = req.getParameter("productName");
+        String idBranch = req.getParameter("branch");
+        Branch branch = new Branch(Integer.parseInt(idBranch));
+        String image = req.getParameter("image");
+        BigDecimal price = BigDecimal.valueOf(Double.parseDouble(req.getParameter("price")));
+        String quantity = req.getParameter("quantity");
+        String warrantyPeriod = req.getParameter("warrantyPeriod");
+        String ram = req.getParameter("ram");
+        String size = req.getParameter("size");
+        String color = req.getParameter("color");
+        String camera = req.getParameter("camera");
+        String operatingSystem = req.getParameter("operatingSystem");
+        String pin = req.getParameter("pin");
+        Product product = new Product(productName, branch, image, price, quantity, warrantyPeriod, ram, size, color, camera, operatingSystem, pin);
+        String pathServerImage = getServletContext().getRealPath("/") + "images";
+        String pathProjectImage  = "F:\\ShopPhoneApp\\src\\main\\webapp\\images";
+
+        String dbImageUrl = null;
+
+        for (Part part : req.getParts()) {
+            String fileName = extractFileName(part);
+
+            if(!fileName.isEmpty()){
+                fileName = new File(fileName).getName();
+
+                if(part.getContentType().equals("image/jpeg")){
+                    part.write(pathProjectImage + File.separator + fileName);
+                    dbImageUrl = File.separator + fileName;
+                    dbImageUrl = dbImageUrl.replace("\\","/");
+                    part.write(pathServerImage + File.separator + fileName);
+                }
+            }
+        }
+        if (dbImageUrl == null) {
+            req.setAttribute("errorImage", "File ảnh không được để trống!");
+        } else {
+            product.setImage(dbImageUrl);
+        }
+        productService.create(product);
         resp.sendRedirect("/product?message=Created");
     }
 
@@ -123,5 +167,16 @@ public class ProductController extends HttpServlet {
         String operatingSystem = req.getParameter("operatingSystem");
         String pin = req.getParameter("pin");
         return new Product(productName, branch, image, price, quantity, warrantyPeriod, ram, size, color, camera, operatingSystem, pin);
+    }
+
+    private String extractFileName(Part part) {
+        String contentDisposition = part.getHeader("content-disposition");
+        String[] items = contentDisposition.split(";");
+        for (String item : items) {
+            if (item.trim().startsWith("filename")) {
+                return item.substring(item.indexOf("=") + 2, item.length() - 1);
+            }
+        }
+        return "";
     }
 }
